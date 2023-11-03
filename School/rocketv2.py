34d662cbccf76d6a488501ef1e1ef97347fd72b5
Will Luxbacher <wluxbacher@hotmail.com>
2023-11-03 12:45:50 -0700

    2

---
+++ School/rocketv2.py
@@ -0,0 +1,33 @@
+def cone():
+    print("     /**\\")
+    print("    //**\\\\")
+    print("   ///**\\\\\\")
+    print("  ////**\\\\\\\\")
+    print(" /////**\\\\\\\\\\")
+
+
+def divider():
+    print("+" + "=*" * 6 + "+")
+
+
+def top():
+    print("|../\\..../\\..|")
+    print("|./\\/\\../\\/\\.|")
+    print("|/\\/\\/\\/\\/\\/\\|")
+
+
+def bottom():
+    print("|\\/\\/\\/\\/\\/\\/|")
+    print("|.\\/\\/..\\/\\/.|")
+    print("|..\\/....\\/..|")
+
+
+cone()
+divider()
+top()
+bottom()
+divider()
+bottom()
+top()
+divider()
+cone()
